85c668e3eda6980ffdfa3530362c2d3a7aa77f9f
alvar <alvaro.monserrat@gmail.com>
2021-04-05 23:57:08 -0400

    Save image

--- app/src/main/java/com/rrat/demo2opencv/MainActivity.java
+++ app/src/main/java/com/rrat/demo2opencv/MainActivity.java
@@ -2,15 +2,21 @@ package com.rrat.demo2opencv;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
@@ -18,10 +24,19 @@ import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST = 1888;
+    String currentPhotoPath;
     private ImageView imageView;
+    private TextView heightText;
+    private TextView widthText;
+
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     @Override
@@ -32,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         OpenCVLoader.initDebug();
 
         this.imageView = findViewById(R.id.image_cv);
+        this.heightText = findViewById(R.id.text_height);
+        this.widthText = findViewById(R.id.text_width);
+
         Button photoButton = findViewById(R.id.button);
         photoButton.setOnClickListener(v -> {
             if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
@@ -41,7 +59,23 @@ public class MainActivity extends AppCompatActivity {
             else
             {
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                }catch (IOException exception){
+                    Log.e("ErrorFile", "Ha ocurrido un error creando el archivo.");
+                }
+
+                if(photoFile != null){
+
+                    Uri photoUri = FileProvider.getUriForFile(this, "com.rrat.fileprovider", photoFile);
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+                }
+
             }
         });
     }
@@ -89,13 +123,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
 
-            Bitmap result = makeGray(photo);
+            galleryAddPic();
+           // Bitmap photo = (Bitmap) data.getExtras().get("data");
 
-            imageView.setImageBitmap(result);
+            //heightText.setText(Integer.toString(photo.getHeight()));
+           // widthText.setText(Integer.toString(photo.getWidth()));
+
+            //Bitmap result = makeGray(photo);
+
+            // imageView.setImageBitmap(result);
+
+
         }
     }
 
+    private File createImageFile() throws IOException{
+
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        //
+        currentPhotoPath = image.getAbsolutePath();
+        Log.i("MainActivity", "Path image: " + currentPhotoPath);
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
 
 }
